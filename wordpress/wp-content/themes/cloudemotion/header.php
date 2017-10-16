51f4014173678010761de17cd2d06c0dd376450a
Alberto <alberto@localhost.localdomain>
2017-10-16 15:58:26 -0400

    ad

--- wordpress/wp-content/themes/cloudemotion/header.php
+++ wordpress/wp-content/themes/cloudemotion/header.php
@@ -19,7 +19,32 @@
 	<link rel="alternate" type="application/rss+xml" title="Startit &raquo; Comments Feed" href="http://startit.select-themes.com/comments/feed/" />
 	<noscript><style type="text/css"> .wpb_animate_when_almost_visible { opacity: 1; }</style></noscript>
 </head>
+<style type="text/css">
+	.loader.in{
+		position: fixed;
+		width: 100vw;
+		height: 100vh;
+		margin: 0 auto;
+		text-align: center;
+		vertical-align: middle;
+		display: inline-block;
+		z-index: 9999;
+		background: white;
+	}
 
+	.loader.in:before{
+		content: "";
+		background-image: url(wp-content/themes/cloudemotion/assets/img/logo1.jpg);
+		width: 300px;
+		height: 300px;
+		background-size:contain; 
+		position: fixed;
+		animation: pulse 2s infinite;
+		left: calc(50% - 150px);
+		top: 30%;
+	}
+
+</style>
 <noscript>
 	<style type="text/css"> .wpb_animate_when_almost_visible { opacity: 1; }</style>
 </noscript>
