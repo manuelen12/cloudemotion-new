58d7ad1b357c6175e20d79fc525153fe3b06e5ef
Alberto <alberto@localhost.localdomain>
2017-10-19 15:34:09 -0400

    2

--- wordpress/wp-content/themes/cloudemotion/header.php
+++ wordpress/wp-content/themes/cloudemotion/header.php
@@ -7,7 +7,7 @@
 	<script id="getdata">
 		window.static="<?php echo get_template_directory_uri(); ?>";
 	</script>
-
+ 
 	<meta charset="UTF-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
