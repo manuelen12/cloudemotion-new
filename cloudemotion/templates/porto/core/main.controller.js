36d61d82eb6600ab6417b1065e5a857d387a36ae
filito <manuel@localhost.localdomain>
2017-10-19 16:25:19 -0400

    Api 50%

--- cloudemotion/templates/porto/core/main.controller.js
+++ cloudemotion/templates/porto/core/main.controller.js
@@ -13,12 +13,81 @@ function MainCtrl($scope,PortfolioService) {
         var top  =  $(link).offset().top;
         body.stop().animate({scrollTop:top},1000,'swing',function(){});
     }
-
-    PortfolioService.getCv().then(function(response){
+    vm.users = {
+        first_name : 'Gerardo José',
+        last_name  : 'Filitto Guerrero',
+        email      : 'gerardofilitto@gmail.com',
+        about_me   : 'Soy responsable en desarrollar tecnología ',
+        birthday   : '24 de Junio',
+        phone      : '+584160450524',
+        skype      : 'filittogerardo',
+        user_nationality: ['Venezolana'],
+        position: {
+            specialty: 'Developer Frontend - Angular JS',
+            sities: 'CloudEmotion',
+            resumen: "Bla bla bla",
+        },
+        user_experience: [
+            {
+                company: {
+                    name: "CloudEmotion CA",
+                    address: "Urbanizacion Valle lindo Sector 3, Calle 3 casa 7 Turmero"
+                },
+                position: {
+                    name: "Gerente"
+                },
+                start_date: "2017-08-06",
+                ending_date: "2017-10-15"
+            },{
+                company: {
+                    name: "EMPTACA",
+                    address: "Urbanizacion Valle lindo Sector 3, Calle 3 casa 7 Turmero"
+                },
+                position: {
+                    name: "Gerente"
+                },
+                start_date: "2017-08-06",
+                ending_date: "2017-10-15"
+            }
+        ],
+        user_education:[
+            {
+                institute: {
+                    name: "UPTA",
+                    address: "La Victoria"
+                },
+                education: {
+                    name: "TSU Informática"
+                },
+                start_date: "2010-07-02",
+                ending_date: "2013-06-30"
+            },
+            {
+                institute: {
+                    name: "UPTA",
+                    address: "La Victoria"
+                },
+                education: {
+                    name: "ING Informática"
+                },
+                start_date: "2013-07-01",
+                ending_date: "2015-07-30"           
+            }
+        ],
+        
+    }
+       
+    
+    //vm.user_exp  = vm.users.user_experience;
+    //vm.user_exp  = vm.user_exp.pop();
+    //vm.education = vm.users.user_education.pop();
+    
+   /* PortfolioService.getCv().then(function(response){
       console.log(response);
+      vm.users = response.data;
     },function(error){
       console.log(error);
-    })
+    })*/
    
     vm.experience = [{
         from:'Sep 2012',
@@ -38,7 +107,7 @@ function MainCtrl($scope,PortfolioService) {
         description: 'laslkdlaskdlñaskdlñaskdlñaskdlñaskdlaskdlñaskdñaskd'
     }]
 
-    vm.education = [{ 
+    /*vm.education = [{ 
             title: 'Tsu. Informatica',
             institute: 'UPTA "Federico Brito Figueroa"',
             years: '2010-2013'             
@@ -46,7 +115,7 @@ function MainCtrl($scope,PortfolioService) {
             title: 'Ing. Informatica',
             institute: 'UPTA "Federico Brito Figueroa"',
             years: '2013-2015',
-    }];
+    }];*/
     
     vm.courses = [{
             title:'course 1',
