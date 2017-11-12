609581fcbe09fb0e7840f77939761c14e54c9df2
luis garcia <rexuzsystems@gmail.com>
2017-11-12 14:50:31 -0400

    hola

--- wordpress/wp-content/themes/cloudemotion/functions.php
+++ wordpress/wp-content/themes/cloudemotion/functions.php
@@ -89,7 +89,7 @@ if(!is_admin()){
 	@ini_set( 'upload_max_size' , '64M' );
 	@ini_set( 'post_max_size', '64M');
 	@ini_set( 'max_execution_time', '300' );
-
+	add_theme_support( 'post-thumbnails' );
 
 
 }
