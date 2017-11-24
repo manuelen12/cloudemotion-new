4e6625cc7da1096b62225e376b256f0706c43e24
manuel <manuel@localhost.localdomain>
2017-11-24 03:33:27 -0400

    s

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'cloudemotion');
+define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'wordpress');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'sector379.aA');
+define('DB_PASSWORD', '12111201');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
