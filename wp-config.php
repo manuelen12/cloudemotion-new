dadf595e6ad6e075f95dc3d1c30c956d8b167e41
manuel <manuel@localhost.localdomain>
2017-11-29 14:31:28 -0400

    s

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'cloudemotion');
+define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'wordpress');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'sector379.aA');
+define('DB_PASSWORD', '12111201');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
@@ -36,6 +36,7 @@ define('DB_CHARSET', 'utf8mb4');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
+
 
 /**#@+
  * Authentication Unique Keys and Salts.
