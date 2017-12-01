de151314c45cdc9ac4fd285972e34c034434458a
filito <manuel@localhost.localdomain>
2017-12-01 15:21:49 -0400

    menu redes

--- wordpress/wp-content/themes/cloudemotion/template-parts/navigation/menu-side.php
+++ wordpress/wp-content/themes/cloudemotion/template-parts/navigation/menu-side.php
@@ -2,9 +2,9 @@
     <div class="social">
         <ul>
             <li><a href="https://www.facebook.com/CloudEmotionLa/" target="_blank" class="icon-facebook fa fa-facebook" title="Facebook"></a></li>
-            <li><a href="https://www.linkedin.com/m/login/" target="_blank" class="icon-twitter fa fa-twitter" title="Twitter"></a></li>
             <li><a href="http://www.googleplus.com/falconmasters" target="_blank" class="icon-googleplus fa fa-linkedin" title="Linkedin"></a></li>
-            <li><a href="mailto:cloudemotioninfo@gmail.com" class="icon-mail fa fa-envelope-o"></a></li>        
+            <li><a href="https://www.linkedin.com/m/login/" target="_blank" class="icon-twitter fa fa-twitter" title="Twitter"></a></li>
+            <li><a href="mailto:cloudemotioninfo@gmail.com" class="icon-mail fa fa-envelope-o" title="{{'Contact us' | translate}}"></a></li>        
         </ul>
     </div>
 </menu-custom>
