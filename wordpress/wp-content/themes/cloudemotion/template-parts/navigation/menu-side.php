082ca1da9076ffc5200ad67dd53eae20a3c07a0b
manuel <manuel@localhost.localdomain>
2017-11-30 23:27:28 -0400

    s

---
+++ wordpress/wp-content/themes/cloudemotion/template-parts/navigation/menu-side.php
@@ -0,0 +1,79 @@
+<menu-custom class="ng-isolate-scope">
+    <header id="header" class="header-floating " data-plugin-options="{'stickyEnabled': false, 'stickyEnableOnBoxed': false, 'stickyEnableOnMobile': false, 'stickyStartAt': 0, 'stickySetTop': '60px', 'stickyChangeLogo': false}">
+    <div class="header-body">
+        <div class="header-container container">
+            <div class="header-row">
+                <div class="header-column">
+                    <div class="header-row">
+                        <div class="header-nav pt-xs">
+                            <button class="btn header-btn-collapse-nav" data-toggle="collapse" data-target=".header-nav-main">
+                                <i class="fa fa-bars"></i>
+                            </button>
+                            <div class="header-nav-main collapse">
+                                <nav class="wrapper-spy">
+                                    <ul class="nav" id="mainNav">
+                                        <li class="active">
+                                            <a data-hash="" class="font-color" ng-click="click('#about-me')">
+                                                <i class="icon-home icons"></i>
+                                                <span class="custom-tooltip ng-binding" ng-bind="'about me' | translate ">about me</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a data-hash="" class="font-color" ng-click="click('#experience')">
+                                                <i class="icon-eye icons"></i>
+                                                <span class="custom-tooltip ng-binding" ng-bind="'Experience' | translate">Experience</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a data-hash="" class="font-color" ng-click="click('#education')">
+                                                <i class="icon-briefcase icons"></i>
+                                                <span class="custom-tooltip ng-binding" ng-bind="'Education' | translate ">Education</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a data-hash="" class="font-color" ng-click="click('#course')">
+                                                <i class="fa fa-edit icons"></i>
+                                                <span class="custom-tooltip ng-binding" ng-bind="'Course' | translate ">Course</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a data-hash="" class="font-color" ng-click="click('#skills')">
+                                                <i class="icon-graduation icons"></i>
+                                                <span class="custom-tooltip ng-binding" ng-bind="'Skills' | translate ">Skills</span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a data-hash="" class="font-color" ng-click="click('#portfolio')">
+                                                <i class="icon-diamond icons"></i>
+                                                <span class="custom-tooltip ng-binding" ng-bind="'Portfolio' | translate ">Portfolio</span>
+                                            </a>
+                                        </li>
+                                                    <!--<li>
+                                                        <a data-hash="" href="#recommendations" class="font-color">
+                                                            <i class="icon-pencil icons"></i>
+                                                            <span class="custom-tooltip">Recommendations</span>
+                                                        </a>
+                                                    </li> -->
+                                                    <li>
+                                                        <a data-hash="" class="font-color" ng-click="click('#blog')">
+                                                            <i class="icon-bubbles icons"></i>
+                                                            <span class="custom-tooltip">Blog</span>
+                                                        </a>
+                                                    </li>
+                                                    <li>
+                                                        <a data-hash="" class="font-color" ng-click="click('#say-hello')">
+                                                            <i class="icon-envelope-open icons"></i>
+                                                            <span class="custom-tooltip ng-binding" ng-bind="'Say Hello' | translate ">Say Hello</span>
+                                                        </a>
+                                                    </li>
+                                                </ul>
+                                            </nav>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    </header>
+</menu-custom>
